817193d120213df7115a5aa0e6559bb7ce534949
Adilet Zainidinov <adiletzainidinov22@gmail.com>
2025-01-31 22:04:42 +0600

    added helpers site

--- src/components/footer-contacts/FooterContacts.jsx
+++ src/components/footer-contacts/FooterContacts.jsx
@@ -10,23 +10,16 @@ import {
 import TelegramIconSecond from '../../assets/images/telegramIconSecond.png';
 import { useSelector } from 'react-redux';
 import { handleSocialClick } from '../../utils/shared/gotuSoccial';
+import { styled, Typography } from '@mui/material';
+import { FaHandHoldingHeart } from 'react-icons/fa';
 
 const FooterContactsDataRu = [
   {
     id: 1,
-    number: '+ 996 770 43 04 76',
-    icon: 'logo-whatsapp',
-    write: 'НАПИСАТЬ WHATSAPP',
-    url: 'https://wa.me/996770430476',
-
-  },
-  {
-    id: 2,
-    number: '+ 996 770 43 04 76',
-    telegram: TelegramIconSecond,
-    write: 'НАПИСАТЬ TELEGRAM',
-    url: 'https://t.me/+996770430476',
-
+    number: 'Наш сайт для поддежки',
+    icon: 'link',
+    write: 'перейти на сайт',
+    url: 'https://details-zeta.vercel.app/',
   },
 ];
 
@@ -57,24 +50,72 @@ const FooterContacts = () => {
     <Container id="footerContacts">
       <StyledBox>
         <BoxContainer>
-          <h2>{languageStore ? 'Контакттар' : 'Контакты'}</h2>
+          <h2 style={{fontSize: 45}}>
+            {languageStore ? 'Контакттар' : 'Как вы можете поддержать нас?'}
+          </h2>
+          <StyledText>
+            Переходите по ссылке и <span>помогите</span> нам сделать больше
+            добра
+          </StyledText>
           <PapaLeftRightFooter>
             {lang.map((item) => (
-              <LeftContactsFooter key={item.id}>
-                <FooterFlex>
-                  <p className="call-out">
-                    <span className="call">
-                      <ion-icon name="call-outline"></ion-icon>
+              <LeftContactsFooter
+                key={item.id}
+                style={{ alignItems: 'center', margin: '0 auto' }}
+              >
+                <FooterFlex
+                  style={{
+                    display: 'flex',
+                    marginTop: 30,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  {/* <p
+                    className="call-out"
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      fontSize: '24px',
+                      position: 'relative',
+                      top: 20
+                    }}
+                  >
+                    <span className="call" style={{ fontSize: 35 }}>
+                      <ion-icon name="information-circle-outline"></ion-icon>
                     </span>
-                    {item.number}
-                  </p>
-                  <ButtonStyle onClick={() => handleSocialClick(item.url)}>
+                    <span style={{ marginTop: -10 }}>{item.number}</span>
+                  </p> */}
+                  <ButtonStyle
+                    onClick={() => handleSocialClick(item.url)}
+                    style={{}}
+                  >
                     <div className="or">
-                      <p>{languageStore ? 'ЖЕ' : 'ИЛИ'}</p>
+                      <p>
+                        {languageStore ? (
+                          'ЖЕ'
+                        ) : (
+                          <FaHandHoldingHeart
+                            style={{
+                              fontSize: '40px',
+                              color: 'white',
+                              width: '50px',
+                              height: 35,
+                              marginTop: 5,
+                            }}
+                          />
+                        )}
+                      </p>
                     </div>
-                    {item.write}
+                    <p style={{ fontSize: '25px' }}>{item.write}</p>{' '}
                     <p className="whatsapp">
-                      {item.icon && <ion-icon name={item.icon}></ion-icon>}
+                      {item.icon && (
+                        <ion-icon
+                          style={{ fontSize: '30px', marginTop: '8px' }}
+                          name={item.icon}
+                        ></ion-icon>
+                      )}
                       {item.telegram && (
                         <img
                           className="telegram"
@@ -95,3 +136,16 @@ const FooterContacts = () => {
 };
 
 export default FooterContacts;
+
+const StyledText = styled(Typography)(() => ({
+  fontSize: '20px',
+  fontWeight: 600,
+  textAlign: 'center',
+  color: '#8c8c8c',
+  marginBottom: '15px',
+
+  '& span': {
+    color: '#ff3b30',
+    fontWeight: 700,
+  },
+}));
